d2b823cbeb74a1428b806a963533531aa64b9cf8
Vorkiuryos <dalvarez@elpuig.xeill.net>
2021-03-25 17:12:44 +0100

    Initial commit

---
+++ src/main/java/org/example/Sprite.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Sprite extends Rectangle2D {
+
+    public Sprite(double v, double v1, double v2, double v3) {
+        super(v, v1, v2, v3);
+    }
+}
