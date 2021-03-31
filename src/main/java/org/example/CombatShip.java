65481676587553062d54f214033d295052265abc
Vorkiuryos <dalvarez@elpuig.xeill.net>
2021-03-31 21:39:22 +0200

    -

---
+++ src/main/java/org/example/CombatShip.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javafx.scene.image.Image;
+
+public class CombatShip extends Sprite{
+
+    public CombatShip(Image image) {
+        super(image);
+    }
+
+}
--- src/main/java/org/example/Sprite.java
+++ src/main/java/org/example/Sprite.java
@@ -1,13 +1,67 @@
 package org.example;
 
 import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Sprite extends Rectangle2D {
+public abstract class Sprite{
+    private Image image;
+    private double width, height, posX, posY;
 
-    public Sprite(double v, double v1, double v2, double v3) {
-        super(v, v1, v2, v3);
+    public void move(){
+        return;
     }
+
+    public void move(String dir){
+        return;
+    }
+
+    public Sprite (Image image){
+        setImage(image);
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    private void setImage(Image image){
+        this.image=image;
+        width = image.getWidth();
+        height = image.getHeight();
+    }
+
+    public void render(GraphicsContext gc){
+        gc.drawImage(image, posX, posY);
+    }
+
+    public void clear(GraphicsContext gc){
+        gc.clearRect(posX, posX, width, height);
+    }
+
+    public Rectangle2D getBoundry(){
+        return new Rectangle2D(posX, posY, width, height);
+    }
+
 }
